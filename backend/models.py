64763ccddbc7e89f7e7bcbb8e42e1b5fab1ec978
MatthewJ Franklin <matthew3j02@gmail.com>
2022-07-21 16:41:05 -0500

    fixing db

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = 'postgresql://postgres:2717@localhost:5432/trivia'
+database_path = 'postgresql://postgres:@localhost:5432/trivia'
 
 db = SQLAlchemy()
 
